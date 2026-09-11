f0ef635ec4e1f301e3f425e8c23a2ff56d76a34f
Bruce Blinn <bruce_blinn@yahoo.com>
2026-09-10 20:31:58 -0700

    Fix whitespace.

--- Age/Age.js
+++ Age/Age.js
@@ -28,7 +28,7 @@ function ageHandler(event) {
 		HTML.putElementValue("error-message-output", error);
 		console.log("Stack trace:", error.stack);
 		document.getElementById("error-message-output")
-			.scrollIntoView({behavior: 'smooth', block: 'start'});
+			.scrollIntoView({ behavior: 'smooth', block: 'start' });
 	}
 }
 
